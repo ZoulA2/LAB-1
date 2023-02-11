1b3398b2c49ca3d7ad12780ee1c99a4bf3079f0c
Z <112178031+ZoulA2@users.noreply.github.com>
2023-02-10 19:53:37 -0500

    3rd attempt on doing the laboratory

---
+++ index.js
@@ -0,0 +1,8 @@
+const reverseSentence = require('@lokeshkatta/reverse-sentence');
+const sentence = "ya pude!";
+const reversed = reverseSentence(sentence);
+console.log(reversed) 
+
+const colors = require('colors');
+ 
+console.log('hello'.green)
